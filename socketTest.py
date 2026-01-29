21db9034738869292a6b6a1c8d62b840e32f6bb6
KarolChudzicki <kcx@techvolver.com>
2026-01-29 13:47:10 +0100

    even more pipette data!

--- socketTest.py
+++ socketTest.py
@@ -7,15 +7,15 @@ from pathlib import Path
 ROBOT_IP = "192.168.0.64"
 PORT = 20001
 
-pipette = "I23670J"
-maxvolume = 5000
+pipette = "M29115B"
+maxvolume = 100
 volume = 10 #In percents
 
 log_path_folder = Path("Logs")
 
 log_path_folder.mkdir(parents=True, exist_ok=True)
 
-txt_file_name = datetime.datetime.now().strftime("%Y-%m-%d %H%M%S") + f" {pipette}_{maxvolume}uL_{volume}.txt"
+txt_file_name = datetime.datetime.now().strftime("%Y-%m-%d %H%M%S") + f" {pipette}_{maxvolume}uL_{volume}%.txt"
 
 log_path = (log_path_folder / txt_file_name)
 
